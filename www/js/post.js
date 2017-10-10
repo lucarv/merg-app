37b19d5bc7d54a9e76a8fd39b03ad091c0b9b495
Tayo Carvalhal <lucarv@microsoft.com>
2017-10-10 10:16:39 +0200

    added delete page

--- www/js/post.js
+++
@@ -1,8 +0,0 @@
-$.post("http://localhost:3000",
-{
-  meterId: "Donald Duck",
-  custId: "Duckburg"
-},
-function(data,status){
-    alert("Data: " + JSON.stringify(data) + "\nStatus: " + status);
-});
